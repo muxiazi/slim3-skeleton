d5c427cc8cf4ae26ed09125ff638a6d438d7567d
Andrew Dyer <andrewdyer@outlook.com>
2019-05-02 14:16:37 +0100

    :sparkles: Added cors middleware

--- bootstrap/middleware.php
+++ bootstrap/middleware.php
@@ -1,5 +1,7 @@
 <?php
 
+$app->add(new Tuupola\Middleware\CorsMiddleware($container->get('config')->get('cors')));
+
 foreach ($container->get('config')->get('middleware') as $middleware) {
     $app->add($container->get($middleware));
 }
